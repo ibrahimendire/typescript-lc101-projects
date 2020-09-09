6754f8795abb3571808251f68f501f0c94627986
ibrahimendire <ibrahimendire@users.noreply.github.com>
2020-09-08 20:51:09 -0500

    studio completed

---
+++ studio/Rocket.ts
@@ -0,0 +1,44 @@
+import { Cargo } from "./Cargo";
+import { Astronaut } from "./Astronaut";
+import { Payload } from "./Payload";
+
+export class Rocket {
+    // properties and methods
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] =[];
+    constructor(name: string,totalCapacityKg: number){
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    sumMass( items: Payload[]): number{
+   let sum: number = 0;
+   for(let i:number = 0; i<items.length;i++){
+       sum +=items[i].massKg;
+   }
+   return sum;
+    }
+    currentMassKg(): number{
+    let AstronautKg: number = this.sumMass(this.astronauts);
+    let CargoKg: number = this.sumMass(this.cargoItems);
+    return CargoKg + AstronautKg;
+    } 
+    canAdd(item:Payload): boolean{
+        return this.currentMassKg() + item.massKg <= this.totalCapacityKg;
+    }
+    addCargo(cargo: Cargo): boolean{
+        if(this.canAdd(cargo)){
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    }
+    addAstronaut(astronaut: Astronaut): boolean{
+        if(this.canAdd(astronaut)){
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false; 
+    } 
+ }
